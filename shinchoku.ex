152b1e9879743a49388340f86f7f47b73577f933
A-gen <agen@seven-words.info>
2015-08-03 15:39:40 +0900

    refactor

--- shinchoku.ex
+++ shinchoku.ex
@@ -2,17 +2,16 @@
 
 defmodule Shinchoku do
 
-  @chars ["進捗", "どう", "です", "か"]
-  @goal Enum.join(@chars)
+  @chars ~w(進捗 どう です か)
+  @goal String.reverse(Enum.join(@chars))
   @length length(@chars)
 
-  def doudesuka(s \\ "") do
-    st = s <> Enum.at(@chars, :random.uniform(@length) - 1)
-    if String.ends_with?(st, @goal), do: st <> "???", else: doudesuka(st)
-  end
+  def doudesuka?, do: doudesuka?("")
+  defp doudesuka?(s = @goal <> _), do: String.reverse(s) <> "???"
+  defp doudesuka?(s), do: doudesuka?(String.reverse(Enum.at(@chars, :random.uniform(@length) - 1)) <> s)
 
 end
 
-s = Shinchoku.doudesuka
+s = Shinchoku.doudesuka?
 IO.puts s
 IO.puts Integer.to_string(String.length(s)) <> "文字で煽られました。"
